f3f224aab7aa715b1a49be0be048bc100d459685
denizyuksl <yukseldnz.33@gmail.com>
2023-12-07 09:41:35 +0300

    Junit practise

--- src/test/java/tests/PRACTISE/P04.java
+++ src/test/java/tests/PRACTISE/P04.java
@@ -5,7 +5,11 @@ import org.checkerframework.dataflow.qual.TerminatesExecution;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
+
+import static org.junit.Assert.assertEquals;
 
 public class P04 extends TestBase {
     @Test
@@ -19,13 +23,53 @@ public class P04 extends TestBase {
     // 3-Üye Kayıt sayfasında "New User Signup!" yazısının göründüğünü doğrulayın
         String expectedIcerik="New User Signup!";
         String actualIcerik=driver.findElement(By.xpath("(//h2)[3]")).getText();
-        Assert.assertEquals(expectedIcerik,actualIcerik);
+       assertEquals(expectedIcerik,actualIcerik);
     // 4-üye kaydı işlemini başlatın
-        driver.findElement(By.xpath("//*[@data-qa='signup-name']")).sendKeys("isim");
-        driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys("dnz@.com");
+        String firstName=faker.name().firstName();
+        String lastName=faker.name().lastName();
+        String email=faker.internet().emailAddress();
+        driver.findElement(By.xpath("//*[@data-qa='signup-name']")).sendKeys(firstName+" "+lastName);
+        driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys(email);
         driver.findElement(By.xpath("//button[@data-qa='signup-button']")).click();
     // 5-Üye kayit formunu doldurun
-    // 6-Üye kaydinin yapildiği mesajının göründğünü doğrulayın
+        driver.findElement(By.id("id_gender1")).click();
+        String password=faker.internet().password();
+        driver.findElement(By.id("password")).sendKeys(password);
+
+        WebElement days= driver.findElement(By.id("days"));
+        Select selectGun=new Select(days);
+        selectGun.selectByValue("5");
+        WebElement months= driver.findElement(By.id("months"));
+        Select selectAy=new Select(months);
+        selectAy.selectByVisibleText("June");
+        WebElement years= driver.findElement(By.id("years"));
+        Select selectYil=new Select(years);
+        selectYil.selectByIndex(1);
+        driver.findElement(By.id("newsletter")).click();
+        driver.findElement(By.id("optin")).click();
+        driver.findElement(By.id("first_name")).sendKeys(firstName);
+        driver.findElement(By.id("last_name")).sendKeys(lastName);
+        driver.findElement(By.id("company")).sendKeys("WQ");
+        String adres=faker.address().streetAddress();
+        driver.findElement(By.id("address1")).sendKeys(adres);
+        WebElement country= driver.findElement(By.id("country"));
+        Select selectCountry=new Select(country);
+        selectCountry.selectByVisibleText("Canada");
+        String state=faker.address().state();
+        driver.findElement(By.id("state")).sendKeys(state);
+        String city=faker.address().city();
+        driver.findElement(By.xpath("//input[@id='city']")).sendKeys(city);
+        String zipCode=faker.address().zipCode();
+        driver.findElement(By.id("zipcode")).sendKeys(zipCode);
+        String mobilePhone=faker.phoneNumber().phoneNumber();
+        driver.findElement(By.id("mobile_number")).sendKeys(mobilePhone);
+        driver.findElement(By.xpath("//button[@data-qa='create-account']")).click();
+        // 6-Üye kaydinin yapildiği mesajının göründğünü doğrulayın
+        WebElement message= driver.findElement(By.xpath("//h2[@data-qa='account-created']"));
+        String actualMessage=message.getText();
+        String expectedMessage="ACCOUNT CREATED!";
+        Assert.assertEquals(expectedMessage,actualMessage);
+
 
     }
 }
